c28a0929f4991bb70de2a8072402fac5119c510c
AdarshGrewal <adarshgrewal@gmail.com>
2023-08-23 14:35:26 +0300

    haydn: Include Leica Camera if exists

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,3 +45,6 @@ include vendor/xiaomi/haydn/BoardConfigVendor.mk
 
 # Firmware
 include vendor/xiaomi/haydn-firmware/BoardConfigVendor.mk
+
+# Inherit from proprietary files for Leica Camera
+-include vendor/xiaomi/haydn-miuicamera/products/board.mk
