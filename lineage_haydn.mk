2aab695d3192754d8a15ec3b54f61aeb596e2dab
Hexdare <mohammedasimuddin786@gmail.com>
2025-06-11 10:15:00 +0000

    haydn: Initialize for EvolutionX 10.X

--- lineage_haydn.mk
+++ lineage_haydn.mk
@@ -16,6 +16,13 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := M2012K11G
 PRODUCT_NAME := lineage_haydn
 
+WITH_GMS := true
+TARGET_DISABLE_EPPE := false
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_BLUR := true
+EVO_BUILD_TYPE := Official
+TARGET_SUPPORTS_QUICK_TAP := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="haydn_global-user 14 UKQ1.231207.002 V816.0.12.0.UKKCNXM release-keys" \
     BuildFingerprint=Xiaomi/haydn_global/haydn:14/UKQ1.231207.002/V816.0.12.0.UKKCNXM:user/release-keys \
